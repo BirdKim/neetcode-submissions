b7c1e6b07925d2d147b3269ee8520745f999b686
Ryan Kim <115209967+BirdKim@users.noreply.github.com>
2026-06-23 15:50:45 -0600

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // use the alphabet way since it only uses array size of 26 for each str
+        Map<String, List<String>> res = new HashMap<>();
+        for (String s : strs) {
+            int[] alph = new int[26];
+            char[] letters = s.toCharArray();
+            for (char c : letters) {
+                alph[c - 'a']++;
+            }
+            String key = Arrays.toString(alph);
+            res.putIfAbsent(key, new ArrayList<>());
+            res.get(key).add(s);
+        }
+        return new ArrayList<>(res.values());
+    }
+}
