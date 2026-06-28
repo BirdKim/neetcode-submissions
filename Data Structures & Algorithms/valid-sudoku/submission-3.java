ef650edd09d19793b6b439bfb92b75892997275a
Ryan Kim <115209967+BirdKim@users.noreply.github.com>
2026-06-28 16:21:13 -0600

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.java
@@ -0,0 +1,37 @@
+// Given solution
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        // checks row
+        for (int row = 0; row < board.length; row++) {
+            // initialize isValid array or set, if contains multiple return false;
+            Set<Character> count = new HashSet<>();
+            for (int i = 0; i < board.length; i++) {
+                if (board[row][i] != '.' && count.contains(board[row][i])) return false;
+                count.add(board[row][i]);
+            }
+        }
+        // checks column
+        for (int col = 0; col < board.length; col++) {
+            // initialize isValid array or set, if contains multiple return false;
+            Set<Character> count = new HashSet<>();
+            for (int i = 0; i < board.length; i++) {
+                if (board[i][col] != '.' && count.contains(board[i][col])) return false;
+                count.add(board[i][col]);
+            }
+        }
+        //checks boxes
+        for (int square = 0; square < board.length; square++) {
+            Set<Character> count = new HashSet<>();
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    int row = (square / 3) * 3 + i;
+                    int col = (square % 3) * 3 + j;
+                    if (board[row][col] != '.' && count.contains(board[row][col])) return false;
+                    count.add(board[row][col]);
+                }
+            }
+        }
+
+        return true;
+    }
+}
