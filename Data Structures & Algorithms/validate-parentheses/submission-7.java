a35411b0be8cff8731fe0ed8074da593374ccf87
Ryan Kim <115209967+BirdKim@users.noreply.github.com>
2026-07-01 15:23:29 -0600

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length()%2 > 0) return false;
+        Stack<Character> openBrackets = new Stack<>();
+        Map<Character, Character> pair = new HashMap<>();
+        pair.put(')', '(');
+        pair.put(']', '[');
+        pair.put('}', '{');
+        for (char c : s.toCharArray()) {
+            // if I put (pair.containsKey(c)) I dont need extra conditionals after the for loop
+            if (pair.containsKey(c)) {
+                if (openBrackets.isEmpty() ||  pair.get(c) != openBrackets.pop())
+                    return false;
+            } else {
+                openBrackets.push(c);
+            }
+        }
+        // this is redundant
+        if (!openBrackets.isEmpty()) return false;
+        return true;
+    }
+}
