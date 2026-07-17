de59a6321603d73af6942496a98ce1120bdd5ec2
Ryan Kim <115209967+BirdKim@users.noreply.github.com>
2026-07-16 21:13:01 -0600

    Add: validate-parentheses - submission-14

---
+++ Data Structures & Algorithms/validate-parentheses/submission-14.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() % 2 > 0) return false;
+        // stack to store open brackets
+        Stack<Character> open = new Stack<>();
+        // need a way to see if the current char is an open bracket for close bracket
+        // use a Map?
+        // if the current char is a close bracket, need a way to check if the peek of the stack
+        // is a pair with the current char
+        Map<Character, Character> pair = new HashMap<>();
+        pair.put(')', '(');
+        pair.put('}', '{');
+        pair.put(']', '[');
+
+        for (int i = 0; i < s.length(); i++) {
+            if (pair.containsKey(s.charAt(i))) {
+                if (!open.isEmpty() && open.peek() == pair.get(s.charAt(i))) {
+                    open.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                open.push(s.charAt(i));
+            }
+        }
+        return open.isEmpty();
+    }
+}
